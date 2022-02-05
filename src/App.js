ef5d3735a385c617efe489da709e1132405d2ced
Hamleto Muradyan <hamomuradyan93@gmail.com>
2022-02-05 11:24:12 +0400

    Changing HTML tags to React components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,27 @@
 import React from 'react';
+import ContainerHeader from './components/jsx/ContainerHeader';
+import Containerfooter from './components/jsx/ContainerFooter';
+import ContainerInstructions from './components/jsx/ContainerInstructions';
+import Cards from './components/jsx/Cards';
+import Scrollbar from './components/jsx/Scrollbar';
 import './App.css';
 import './components/css/container.css'
 import './components/css/container__main.css'
-import './components/css/container__header.css'
 import './components/css/container__center.css'
-import './components/css/cards.css'
-import './components/css/scrollbar.css'
-import './components/css/container__footer.css'
-import './components/css/container__instructions.css'
 
 const App = () => {
   return (
     <div className='App'>
       <div className='container'>
         <div className='container__main'>
-          <div className='container__header'>
-
-          </div>
+          <ContainerHeader />
           <div className='container__center'>
-            <div className='cards'>
-
-            </div>
-            <div className='scrollbar'>
-
-            </div>
+            <Cards />
+            <Scrollbar />
           </div>
-          <div className='container__footer'>
-
-          </div>
+          <Containerfooter />
         </div>
-        <div className='container__instructions'>
-
-        </div>
+        <ContainerInstructions />
       </div>
     </div>
   );
