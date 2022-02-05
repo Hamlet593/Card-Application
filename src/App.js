65bbb071c334c2c05db2d995d96222a53e2b6b5b
Hamleto Muradyan <hamomuradyan93@gmail.com>
2022-02-05 13:16:43 +0400

    Adding overflow css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ContainerHeader from './components/jsx/ContainerHeader';
 import Containerfooter from './components/jsx/ContainerFooter';
 import ContainerInstructions from './components/jsx/ContainerInstructions';
-import Cards from './components/jsx/Cards';
-import Scrollbar from './components/jsx/Scrollbar';
+import CardsList from './components/jsx/CardsList';
 import './App.css';
 import './components/css/container.css'
 import './components/css/container__main.css'
 import './components/css/container__center.css'
 
 const App = () => {
+
+  const [cards, setcards] = useState([]);
+
   return (
     <div className='App'>
       <div className='container'>
         <div className='container__main'>
           <ContainerHeader />
           <div className='container__center'>
-            <Cards />
-            <Scrollbar />
+            <CardsList />
           </div>
           <Containerfooter />
         </div>
